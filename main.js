7a30cbb4403c5247ad9af363ef954626c2b4c68b
reshmi-02 <reshmihash123@gmail.com>
2023-07-20 10:53:34 +0530

    addnew

---
+++ main.js
@@ -0,0 +1,10 @@
+document.write("c)table"+"<br>");
+var a=1;
+var b=prompt("Enter a table you want");
+var n=prompt("Enter a last value");
+var c;
+while(a<=n){
+	c=a*b;
+	document.write(a+"x"+b+"="+c+"<br>");
+	a++;
+}
